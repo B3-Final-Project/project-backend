4c52354e2938d40140382eb5ae6a0a97291afc87
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2025-04-22 08:47:46 +0200

    feat: removed auth process to use self hosted aws cognito solution (#28)
    * feat: implement getUserInfo method in AuthService to retrieve user details from Cognito
    
    * refactor: remove AuthModule and AuthController from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CognitoStrategy } from './auth/cognito.strategy';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
 import { PreferenceModule } from './preferences/preference.module';
 import { Constants } from './constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -23,10 +21,9 @@ import { Preference } from './common/entities/preference.entity';
       database: Constants.DATABASE_NAME,
       synchronize: true,
     }),
-    AuthModule,
     PreferenceModule,
   ],
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   providers: [AppService, CognitoStrategy],
 })
 export class AppModule {}
--- src/auth/cognito.strategy.ts
+++ src/auth/cognito.strategy.ts
@@ -1,45 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import * as jwkPem from 'jwk-to-pem';
-import * as jwt from 'jsonwebtoken';
-import axios from 'axios';
-import * as process from 'node:process';
+import * as jwksRsa from 'jwks-rsa';
 
 @Injectable()
 export class CognitoStrategy extends PassportStrategy(Strategy, 'jwt') {
-  private readonly cognitoIssuer: string;
-  private pems: { [key: string]: string } = {};
-  private readonly cognitoUserPoolID = process.env.COGNITO_USER_POOL_ID;
-
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKeyProvider: (request, rawJwtToken, done) => {
-        const decodedToken = jwt.decode(rawJwtToken, { complete: true });
-        const kid = decodedToken?.header.kid;
-        const pem = this.pems[kid];
-        done(null, pem);
-      },
+      issuer: process.env.COGNITO_USER_POOL,
+      algorithms: ['RS256'],
+      secretOrKeyProvider: jwksRsa.passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 10,
+        jwksUri: `${process.env.COGNITO_USER_POOL}/.well-known/jwks.json`,
+      }),
     });
-    this.cognitoIssuer = `https://cognito-idp.eu-west-3.amazonaws.com/${this.cognitoUserPoolID}`;
-    this.loadPems();
   }
-
-  private loadPems = async () => {
-    const url = `${this.cognitoIssuer}/.well-known/jwks.json`;
-    const { data } = await axios.get(url);
-    data.keys.forEach((key) => {
-      this.pems[key.kid] = jwkPem(key);
-    });
-  };
 
   async validate(payload: any) {
     return {
-      userId: payload.sub,
+      email: payload.email,
       username: payload['cognito:username'],
-      roles: payload['cognito:groups'] ?? [],
     };
   }
 }
