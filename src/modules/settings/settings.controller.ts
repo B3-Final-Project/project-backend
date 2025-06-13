3f87c6bd0e421128c4bd08a6a8ed32f57b57e5b1
CorentinBL <124254584+CorentinBL@users.noreply.github.com>
2025-06-13 09:45:31 +0200

    SIngle quotes for import (#42)

--- src/modules/settings/settings.controller.ts
+++ src/modules/settings/settings.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthSettingsDto } from './dto/auth-settings.dto';
-import {ApiOkResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
+import {ApiOkResponse, ApiOperation, ApiTags} from '@nestjs/swagger';
 
 @ApiTags('settings')
 @Controller('settings')
